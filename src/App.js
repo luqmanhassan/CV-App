845fa7a1cf4882c6212ebeb6987c61252e12429e
Luqman Hassan <reallookmanlook@gmail.com>
2021-09-05 20:21:04 -0700

    Used Styled Comp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-// import React, {Component} from 'react';
 import React from 'react';
 import Education from './components/education.js';
 import Experience from './components/experience.js';
 import General from './components/general.js';
 import './styles/styles.css';
+import styled from 'styled-components';
 
+const Main = styled.div`
+  border: 5px solid black;
+  margin: 50px auto;
+  max-width: 50%;
+  padding: 5px;
+`;
+
+const H1 = styled.h1`
+  text-decoration: underline;
+  font-size: 50px;
+  text-align: center;
+`;
+
+const BTN = styled.button`
+  margin: 0px auto;
+  display: block;
+  padding: 5px 15px;
+  border: 3px solid black;
+
+  &:hover {
+    background-color: black;
+    color: white;
+    cursor: pointer;
+  }
+`;
 function App() {
   return (
-    <div className="main">
-      <h1>CV APP</h1>
+    <Main>
+      <H1>CV APP</H1>
       <General />
       <Education />
       <Experience />
-      <button
+      <BTN
         id="printbtn"
         onClick={() => {
           window.print();
         }}
       >
         Print
-      </button>
-    </div>
+      </BTN>
+    </Main>
   );
 }
 

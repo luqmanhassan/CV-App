33f300410b542e09cb54acb847046f4404bd10ef
Luqman Hassan <reallookmanlook@gmail.com>
2021-09-14 15:40:18 -0700

    included custom theme

--- src/App.js
+++ src/App.js
@@ -6,30 +6,46 @@ import './styles/styles.css';
 // MUI Bellow
 import {Typography, Container, Button} from '@mui/material';
 import PrintIcon from '@mui/icons-material/Print';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ffac1e',
+    },
+    secondary: {
+      main: '#3cff66',
+    },
+  },
+  typography: {
+    fontFamily: ['Times New Roman', 'Arial', 'sans-serif'].join(','),
+  },
+});
 function App() {
   return (
-    <Container maxWidth="md">
-      <Typography variant="h1" sx={{textAlign: 'center', margin: '20px 0px'}}>
-        CV App
-      </Typography>
-      <General />
-      <Education />
-      <Experience />
-      <Button
-        id="printbtn"
-        variant="contained"
-        size="large"
-        startIcon={<PrintIcon />}
-        color="primary"
-        sx={{display: 'flex', justifyContent: 'center'}}
-        onClick={() => {
-          window.print();
-        }}
-      >
-        Print
-      </Button>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="md" sx={{backgroundColor: 'primary.main'}}>
+        <Typography variant="h1" sx={{textAlign: 'center', margin: '20px 0px'}}>
+          CV App
+        </Typography>
+        <General />
+        <Education />
+        <Experience />
+        <Button
+          id="printbtn"
+          variant="contained"
+          size="large"
+          startIcon={<PrintIcon />}
+          color="secondary"
+          sx={{mx: 'auto', display: 'flex'}}
+          onClick={() => {
+            window.print();
+          }}
+        >
+          Print
+        </Button>
+      </Container>
+    </ThemeProvider>
   );
 }
 

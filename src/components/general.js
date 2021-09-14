19e470a38b2bc994b33cbbb78c8f2b3de9c42c27
Luqman Hassan <reallookmanlook@gmail.com>
2021-09-13 17:36:57 -0700

    Switched to MUI

--- src/components/general.js
+++ src/components/general.js
@@ -1,32 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
-const GenInfo = styled.div`
-  margin: auto;
-  width: 400px;
-  display: none;
-`;
-const GeneralComponent = styled.div`
-  margin: 20px 0;
-`;
-
-const Form = styled.form`
-  display: grid;
-  grid-template-columns: auto auto;
-  justify-content: space-around;
-`;
-
-const Button = styled.button`
-  margin: 0px auto;
-  display: block;
-  padding: 5px 15px;
-  border: 3px solid black;
-
-  &:hover {
-    background-color: black;
-    color: white;
-    cursor: pointer;
-  }
-`;
+import {Typography, Container, Button, TextField, Stack} from '@mui/material';
 
 class General extends React.Component {
   constructor(props) {
@@ -34,86 +7,119 @@ class General extends React.Component {
     this.state = {fname: null, lname: null, email: null, phone: null};
   }
 
-  displayInfo = (ev) => {
-    ev.target.style.display = 'none';
-    document.getElementById('gen_Info').style.display = 'block';
-    document.getElementById('generalDiv').style.display = 'none';
-  };
-
   render() {
     return (
-      <GeneralComponent class="generalComponent">
-        <h3>General Info</h3>
-        <Form className="generalDiv" id="generalDiv">
-          <label>First Name</label>
-          <input
-            type="text"
+      <Container maxWidth="lg" class="generalComponent">
+        <Typography variant="h5">General Info</Typography>
+        <Stack
+          component="form"
+          className="generalDiv"
+          id="generalDiv"
+          spacing={2}
+          sx={{
+            justifyContent: 'center',
+            alignItems: 'flex-end',
+          }}
+        >
+          <TextField
+            label="First Name"
+            variant="outlined"
+            size="small"
             id="firstName_input"
             onChange={(ev) => {
               this.setState({fname: ev.target.value});
             }}
-            placeholder="First Name"
           />
-          <label>Last Name</label>
-          <input
-            type="text"
+          <TextField
+            label="Last Name"
+            variant="outlined"
+            size="small"
             id="lastName_input"
             onChange={(ev) => {
               this.setState({lname: ev.target.value});
             }}
-            placeholder="Last Name"
           />
-          <label>Email</label>
-          <input
-            type="text"
+          <TextField
+            label="Email"
+            variant="outlined"
+            size="small"
             id="email_input"
             onChange={(ev) => {
               this.setState({email: ev.target.value});
             }}
-            placeholder="Email"
           />
-          <label>Phone #</label>
-          <input
-            type="text"
+          <TextField
+            label="Number"
+            variant="outlined"
+            size="small"
             id="phone_input"
             onChange={(ev) => {
               this.setState({phone: ev.target.value});
             }}
-            placeholder="Phone #"
           />
-        </Form>
+        </Stack>
         <Info
           fname={this.state.fname}
           lname={this.state.lname}
           email={this.state.email}
           phone={this.state.phone}
         />
-        <Button id="saveGeneral" onClick={this.displayInfo}>
+        <Button
+          variant="contained"
+          id="saveGeneral"
+          onClick={(ev) => {
+            ev.target.style.display = 'none';
+            document.getElementById('gen_Info').style.display = 'flex';
+            document.getElementById('generalDiv').style.display = 'none';
+            document.getElementById('editBTN').style.display = 'block';
+          }}
+          sx={{
+            marginLeft: '50%',
+            marginRight: '50%',
+          }}
+        >
           Save
         </Button>
-      </GeneralComponent>
+        <Button
+          id="editBTN"
+          variant="contained"
+          onClick={(ev) => {
+            ev.target.style.display = 'none';
+            document.getElementById('generalDiv').style.display = 'flex';
+            document.getElementById('gen_Info').style.display = 'none';
+            document.getElementById('saveGeneral').style.display = 'block';
+          }}
+          sx={{
+            display: 'none',
+            marginLeft: '50%',
+            marginRight: '50%',
+          }}
+        >
+          Edit
+        </Button>
+      </Container>
     );
   }
 }
 
 function Info(props) {
   return (
-    <GenInfo className="gen_Info" id="gen_Info">
-      <p>First Name: {props.fname}</p>
-      <p>Last Name: {props.lname}</p>
-      <p>Email: {props.email}</p>
-      <p>Phone #: {props.phone}</p>
-      <button
-        id="editBTN"
-        onClick={() => {
-          document.getElementById('generalDiv').style.display = 'grid';
-          document.getElementById('gen_Info').style.display = 'none';
-          document.getElementById('saveGeneral').style.display = 'block';
-        }}
-      >
-        Edit
-      </button>
-    </GenInfo>
+    <Stack
+      maxWidth="lg"
+      className="gen_Info"
+      id="gen_Info"
+      spacing={2}
+      sx={{
+        display: 'none',
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end',
+      }}
+    >
+      <Typography variant="body1">First Name: {props.fname}</Typography>
+      <Typography variant="body1">Last Name: {props.lname}</Typography>
+      <Typography variant="body1">Email: {props.email}</Typography>
+      <Typography variant="body1">Phone #: {props.phone}</Typography>
+    </Stack>
   );
 }
 
